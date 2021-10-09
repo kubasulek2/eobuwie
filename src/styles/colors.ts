2ff6b184377726f028784b83f7a1fa3d644aef88
kubasulek2 <kubasulek2@gmail.com>
2021-10-09 15:30:15 +0200

    Add color names as constants

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+/* Global css colors class names  */
+export const cyan = "cyan";
+export const cyan_bg = "cyan_bg";
+export const cyan_light = "cyan_light";
+export const cyan_light_bg = "cyan_light_bg";
+export const red_bg = "red_bg";
+export const teal = "teal";
+export const teal_bg = "teal_bg";
+export const teal_light = "teal_light";
+export const teal_light_bg = "teal_light_bg";
+export const gray_bg = "gray_bg";
+export const gray_light_bg = "gray_light_bg";
+export const white_bg = "white_bg";
+export const text_dark = "text_dark";
+export const text_medium = "text_medium";
+export const text_white = "text_white";
