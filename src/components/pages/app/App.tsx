54c4ff88b7e12c57406d382ae33af4595eeac883
kubasulek2 <kubasulek2@gmail.com>
2021-10-10 15:42:31 +0200

    Adjust global styles, add App styles

--- src/components/pages/app/App.tsx
+++ src/components/pages/app/App.tsx
@@ -1,11 +1,14 @@
 import {VFC} from "react";
+import styles from "./App.module.css";
 import Divider from "../../atoms/divider/Divider";
 import StarRating from "../../molecules/star_rating/StarRating";
+import {joinClassNames} from "../../../utils/joinClassNames";
+import {gray_light_bg} from "../../../styles/colors";
 
 
 const App: VFC = () => {
   return (
-    <div style={{display: "flex"}}>
+    <div className={joinClassNames(styles.app, gray_light_bg)}>
       <StarRating score={4.5} votes={100} size="medium" />
       <Divider />
     </div>
