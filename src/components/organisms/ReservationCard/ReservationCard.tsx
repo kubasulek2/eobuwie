d7c5a885eb69adab74b087eea83520faf1c3e665
kubasulek2 <kubasulek2@gmail.com>
2021-10-11 09:32:25 +0200

    Add accessible error display

--- src/components/organisms/ReservationCard/ReservationCard.tsx
+++ src/components/organisms/ReservationCard/ReservationCard.tsx
@@ -1,4 +1,5 @@
 import {VFC, FormEvent, useState} from 'react';
+import {joinClassNames} from "../../../utils/joinClassNames";
 import Button from "../../atoms/button/Button";
 import Divider from "../../atoms/divider/Divider";
 import Input from "../../atoms/input/Input";
@@ -8,12 +9,16 @@ import styles from './ReservationCard.module.css';
 import {ReservationCardProps} from "./types";
 
 const ReservationCard: VFC<ReservationCardProps> = ({price, score, votes, onSubmit, startDate}) => {
+  /* Displays form errors, indicates validity of the form */
+  const [formError, setFormError] = useState("");
+
   /* Call prevent default, as page should not be reloaded */
   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
     onSubmit();
   };
-  const [date, setDate] = useState("");
+
+
   return (
     <div className={styles.card}>
       <ReservationHeader price={price} score={score} votes={votes} />
@@ -27,8 +32,13 @@ const ReservationCard: VFC<ReservationCardProps> = ({price, score, votes, onSubm
           <Label idFor="dummy1" title="Dummy 2" size="small" />
           <Input id="dummy2" placeholder="Dummy content 2" value="" onChange={() => {}} />
         </div>
+
         <div className={styles.formControl}>
-          <Button type="submit" size="medium" onClick={() => {}}>Submit</Button>
+          <p 
+            className={joinClassNames(styles.errMsg, formError ? styles.error : "")}>
+            {formError}
+            </p>
+          <Button type="submit" size="medium" disabled={Boolean(formError)}>Submit</Button>
         </div>
       </form>
     </div>
