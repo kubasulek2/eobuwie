05d176765f5358f2a04c5899ea7381cc5d634526
kubasulek2 <kubasulek2@gmail.com>
2021-10-14 18:03:12 +0200

    Add usePrevious

---
+++ src/hooks/usePrevious.ts
@@ -0,0 +1,10 @@
+import {useEffect, useRef} from 'react';
+
+export const usePrevious = <T = any> (value: T) => {
+  const ref = useRef<T | undefined>();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  return ref.current;
+};
